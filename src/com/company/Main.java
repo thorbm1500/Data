7a728329af135d431079accc37fb47acb02c52c4
thorbm1500 <43374833+thorbm1500@users.noreply.github.com>
2021-09-07 10:14:21 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String day = "Tuesday";
+        String date = "7th";
+        String month = "September";
+        String year = "2021";
+        System.out.println(day+", "+month+" "+date+", "+year);
+        System.out.print(day+" "+date+" "+month+" "+year);
+	// write your code here
+    }
+}
